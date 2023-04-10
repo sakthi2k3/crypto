8cb68cfa0ed9bbff2f25a50735d826b9dc5bb467
sakthi2k3 <71747380+sakthi2k3@users.noreply.github.com>
2023-04-10 23:40:53 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import { makeStyles } from "@material-ui/core";
+import Homepage from "./Pages/HomePage";
+import "./App.css";
+import { BrowserRouter, Route } from "react-router-dom";
+import CoinPage from "./Pages/CoinPage";
+import Header from "./components/Header";
+import Alert from "./components/Alert";
+import news from "./Pages/news";
+
+
+const useStyles = makeStyles(() => ({
+  App: {
+    backgroundColor: "black",
+    color: "white",
+    minHeight: "100vh",
+  },
+}));
+
+function App() {
+  const classes = useStyles();
+
+  return (
+    <BrowserRouter>
+      <div className={classes.App}>
+        <Header />
+        <Route path="/" component={Homepage} exact />
+        <Route path="/coins/:id" component={CoinPage} exact />
+        <Route path="/news" component={news} exact />
+      </div>
+      <Alert />
+    </BrowserRouter>
+  );
+}
+
+export default App;
